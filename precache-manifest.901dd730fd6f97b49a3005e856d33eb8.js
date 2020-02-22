9d3d0f0d9400029f8ff63e95d5cb5bb497a647e0
Halyna Halchuk <halchuk@fabrica.net.ua>
2020-02-22 23:30:51 +0200

    Updates

---
+++ precache-manifest.901dd730fd6f97b49a3005e856d33eb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5447238347c7dc063cf0ddfa33efdfbc",
+    "url": "/learn-react/index.html"
+  },
+  {
+    "revision": "277b7b8e33c679816c95",
+    "url": "/learn-react/static/css/main.e1d0d93e.chunk.css"
+  },
+  {
+    "revision": "04fa56a4f63caae61e8e",
+    "url": "/learn-react/static/js/2.3513fd6d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/learn-react/static/js/2.3513fd6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "277b7b8e33c679816c95",
+    "url": "/learn-react/static/js/main.09e0a526.chunk.js"
+  },
+  {
+    "revision": "1a6298ad1f4ff31a3ed1",
+    "url": "/learn-react/static/js/runtime-main.82700265.js"
+  }
+]);
